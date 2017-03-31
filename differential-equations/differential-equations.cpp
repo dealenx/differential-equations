859724e2c97155de13875f8bf704cf66132b2108
Daniil Gorodiloff <dealenx@gmail.com>
2017-03-31 18:38:37 +0700

    0.0.2

--- differential-equations/differential-equations.cpp
+++ differential-equations/differential-equations.cpp
@@ -5,6 +5,24 @@
 
 
 int main() {
-    return 0;
+	double a, b, n, h;
+	
+
+	printf("Enter a,b, n: ");
+	scanf_s("%lf %lf %lf",&a, &b, &n);
+	h = (b - a) / n;
+	printf("a - %lf, b - %lf, n - %lf, h - %lf \n", a, b, n, h);
+
+	double *x = new double[n];
+
+	for (int i = 1; i < n + 1; i++) {
+		x[i] = a + (i*h);
+		printf("x[%d]=%lf ",i,x[i]);
+	}
+
+	
+
+	
+	return 0;
 }
 
